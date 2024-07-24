91a5294207b9a88f4d1cc7f28e91f1d093449ba5
nyximos <nyximos@gmail.com>
2024-07-24 13:59:34 +0900

    refactor: add builder

--- src/test/java/com/wanted/recruitment/converter/RecruitmentConverterTest.java
+++ src/test/java/com/wanted/recruitment/converter/RecruitmentConverterTest.java
@@ -23,20 +23,21 @@ class RecruitmentConverterTest {
     @Test
     @DisplayName("[RecruitmentConverter][convert][Success]")
     void convertTest() {
-        RecruitmentRequestModel requestModel = new RecruitmentRequestModel();
         long companyId = 1L;
         BigDecimal compensation = new BigDecimal("1000000");
         String position = "백엔드 주니어 개발자";
         String technology = "Python";
         String contents = "원티드랩에서 백엔드 주니어 개발자를 채용합니다.";
 
-        requestModel.setCompanyId(companyId);
-        requestModel.setPosition(position);
-        requestModel.setCompensation(compensation);
-        requestModel.setTechnology(technology);
-        requestModel.setContents(contents);
+        RecruitmentRequestModel model = RecruitmentRequestModel.builder()
+                .companyId(companyId)
+                .position(position)
+                .compensation(compensation)
+                .technology(technology)
+                .contents(contents)
+                .build();
 
-        RecruitmentEntity result = recruitmentConverter.convert(requestModel);
+        RecruitmentEntity result = recruitmentConverter.convert(model);
         assertNull(result.getId());
         assertEquals(position, result.getPosition());
         assertEquals(compensation, result.getCompensation());
